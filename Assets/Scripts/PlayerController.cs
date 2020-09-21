51e0c41bef962a80a3ca035897e1494e03a3b41b
Praneil Kamat <pkamat@mac.com>
2020-09-21 19:48:34 +0100

    add ability to jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private LayerMask platformsLayerMask;
+
+    private Rigidbody2D rigidbody2d; 
+    private BoxCollider2D boxCollider2d;
+
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 50f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
     }
 }
